18f02e94e83c9b82871a6652cfce6b157aba3f33
suphanatchanlek <suphanatchanlek@gmail.com>
2024-12-24 13:24:41 +0700

    add setup file other and router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router";
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
 )
