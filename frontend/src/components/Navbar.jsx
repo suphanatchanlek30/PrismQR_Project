126b031ebbd4036aa86cb382c913af4499562af3
suphanatchanlek <suphanatchanlek@gmail.com>
2024-12-24 15:37:37 +0700

    add Home page and update Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// import Logo from "../assets/Logo/logo-noback.svg";
 
 const Navbar = () => {
   return (
@@ -19,7 +18,7 @@ const Navbar = () => {
         </div>
 
         {/* Main Navbar */}
-        <nav className='flex justify-between items-center px-6 h-[80px] bg-[#000]'>
+        <nav className='relative flex justify-between items-center px-6 h-[80px] bg-[#000] border-b border-b-[#ffffff43] z-50'>
             {/* Logo */}
             <div className="flex items-center text-[#FFFFFF] text-[28px] font-body2 font-medium">
                 Primes QR
@@ -27,15 +26,15 @@ const Navbar = () => {
 
             {/* Main Menu Right */}
             <div className='flex items-center gap-6'>
-            <ul className="hidden md:flex items-center space-x-6 font-normal tracking-[0.5px]">
-                <li><a href="#" className="hover:underline text-[#FFFFFF] text-[14px]">HOME</a></li>
-                <li><a href="#" className="hover:underline text-[#FFFFFF] text-[14px]">DOCS</a></li>
-            </ul>
+                <ul className="hidden md:flex items-center space-x-6 font-normal tracking-[0.5px]">
+                    <li><a href="#" className="hover:text-[#01cc04ea] text-[#FFFFFF] text-[14px] duration-100">HOME</a></li>
+                    <li><a href="#" className="hover:text-[#01cc04ea] text-[#FFFFFF] text-[14px] duration-100">DOCS</a></li>
+                </ul>
 
-            {/* Button */}
-            <button className="bg-[#01CC03] px-[16px] py-[12px] text-[14px] font-semibold rounded-[4px] hover:bg-[#01cc04ea] focus:outline-none tracking-[0.5px]">
-                CONTACT
-            </button>
+                {/* Button */}
+                <button className="bg-[#01CC03] px-[16px] py-[12px] text-[14px] font-semibold rounded-[4px] hover:bg-[#01cc04ea] focus:outline-none tracking-[0.5px] duration-100">
+                    CONTACT
+                </button>
             </div>
         </nav>
     </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+
+        <section className="relative flex font-body2 items-center justify-center bg-black text-white py-20 px-4">
+            <div className='absolute green-glow'></div>
+
+            <div className="max-w-4xl h-[1080px] text-center mt-10">
+                {/* ข้อความบรรทัดแรก */}
+                <h1 className="relative text-3xl sm:text-4xl md:text-6xl font-bold uppercase leading-tight tracking-wide mb-4 z-50">
+                    The fastest and the most powerful <span className='text-[#01CC03]'>QR code</span> generator
+                </h1>
+
+                {/* form */}
+                <form className="url relative font-body2 flex flex-col items-center justify-center mt-[100px] z-50 max-w-md w-full mx-auto">
+                    {/* Label */}
+                    <label  className="block text-left w-full text-sm md:text-base font-medium text-gray-100 mb-2">
+                        Enter your URL
+                    </label>
+                    {/* Input */}
+                    <div className='url flex flex-row max-w-md w-full mx-auto gap-2'>
+                        <input
+                            type="url"
+                            id="url"
+                            name="url"
+                            placeholder="https://example.com"
+                            className="w-full p-3 rounded-md border text-black focus:outline-none focus:ring-2 focus:ring-[#01cc04bf] transition duration-300"
+                            required
+                        />
+                        {/* Dropdown เลือกสกุลไฟล์ */}
+                        <select
+                            id="extensionSelect"
+                            className="border text-[#000] rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#01cc04bf] duration-300"
+                        >
+                            <option value=".png">.png</option>
+                            <option value=".jpg">.jpg</option>
+                        </select>
+                    </div>
+                </form>
+
+                {/* ปุ่ม Submit */}
+                <button
+                    type="submit"
+                    className="mt-6 px-6 py-3 bg-[#01CC03] text-black font-semibold rounded-md hover:bg-[#01cc04ea] transition"
+                >
+                    Submit
+                </button>
+
+            </div>
+
+        </section>
+    )
 }
 
 export default Home
